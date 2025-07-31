82351a1a5078865279b315a9e25f0428ffc5d704
Paulus Kassapa <paulussuherman1@gmail.com>
2025-07-31 11:00:01 +0700

    feat(talent-growth): show movie release date, rating, and overview

--- src/App.jsx
+++ src/App.jsx
@@ -18,13 +18,29 @@ function App() {
       <h1>Popular Movies</h1>
       <div style={{ display: "flex", flexWrap: "wrap" }}>
         {movies.map((movie) => (
-          <div key={movie.id} style={{ margin: 10, width: 200 }}>
+          <div
+            key={movie.id}
+            style={{
+              margin: 10,
+              width: 200,
+              border: "1px solid #ddd",
+              padding: 10,
+              borderRadius: 8,
+            }}
+          >
             <img
               src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
               alt={movie.title}
-              style={{ width: "100%" }}
+              style={{ width: "100%", borderRadius: 5 }}
             />
-            <p>{movie.title}</p>
+            <p style={{ fontWeight: "bold", margin: "10px 0 5px" }}>
+              {movie.title}
+            </p>
+            <p style={{ margin: 0 }}>📅 {movie.release_date}</p>
+            <p style={{ margin: 0 }}>⭐ {movie.vote_average}</p>
+            <p style={{ fontSize: 12, color: "#555" }}>
+              {movie.overview ? movie.overview.substring(0, 100) + "..." : ""}
+            </p>
           </div>
         ))}
       </div>
